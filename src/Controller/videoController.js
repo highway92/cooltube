16195cbaadf7d8ac4c2483f01771b72b989fdd5b
highway92 <highway@highway92ui-MacBookAir.local>
2021-07-05 03:06:22 +0900

    오류수정2

--- src/Controller/videoController.js
+++ src/Controller/videoController.js
@@ -75,8 +75,8 @@ export const postUpload = async (req, res) => {
     const newVideo = await Video.create({
       title,
       description,
-      fileUrl: video[0].location,
-      thumbUrl: thumb[0].location,
+      fileUrl: isHeroku ? video[0].location : video[0].path,
+      thumbUrl: isHeroku ? thumb[0].location : video[0].path,
       owner: _id,
       hashtags: Video.formatHashtags(hashtags),
     });
@@ -85,7 +85,6 @@ export const postUpload = async (req, res) => {
     user.save();
     return res.redirect("/");
   } catch (error) {
-    console.log(error);
     return res.status(400).render("upload", {
       pageTitle: "Upload Video",
       errorMessage: error._message,
