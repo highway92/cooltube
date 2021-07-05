6054074bab7f18fd7b839893cbde1aed61d080a4
highway92 <highway@highway92ui-MacBookAir.local>
2021-07-05 15:31:53 +0900

    MONGOURL수정

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.Mongo_URL, {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
