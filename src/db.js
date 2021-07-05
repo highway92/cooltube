3be95502faf936726a3cb7fb1f2fa3d5dd0a2ba6
highway92 <highway@highway92ui-MacBookAir.local>
2021-07-05 15:28:20 +0900

    mongo URL 수정

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.Mongo_atlas, {
+mongoose.connect(process.env.Mongo_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
